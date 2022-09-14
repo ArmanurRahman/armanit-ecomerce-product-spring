fffe9b3772b78fc848bfc90660a362251d68faed
Armanur Rahman <mubeenarman19@gmail.com>
2022-09-14 21:54:22 +0900

    add get product api

--- src/main/java/net/armanit/ecommerce/product/controller/ProductController.java
+++ src/main/java/net/armanit/ecommerce/product/controller/ProductController.java
@@ -4,23 +4,35 @@ import lombok.extern.slf4j.Slf4j;
 import net.armanit.ecommerce.product.model.Product;
 import net.armanit.ecommerce.product.repository.ProductInterface;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
-@RestController
+@RestController()
 @Slf4j
+@RequestMapping("product")
 public class ProductController {
 
     @Autowired
     ProductInterface productInterface;
 
-    @PostMapping("/product/add")
+    @PostMapping("/add")
     public Product addProduct(@Valid @RequestBody Product product) {
         log.info("Product add");
         return  productInterface.save(product);
+    }
+
+    @GetMapping("/getAllProducts")
+    public List<Product> getAllProduct() {
+        log.info("get all product");
+        return productInterface.findAll();
+    }
+
+    @GetMapping("/getProductById/{productId}")
+    public Product getProductById(@PathVariable String productId) {
+        log.info("getProductById");
+        return productInterface.findProductById(productId);
     }
 
 }
--- src/main/java/net/armanit/ecommerce/product/repository/ProductInterface.java
+++ src/main/java/net/armanit/ecommerce/product/repository/ProductInterface.java
@@ -4,4 +4,5 @@ import net.armanit.ecommerce.product.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductInterface extends MongoRepository<Product, String> {
+    public Product findProductById(String productId);
 }
